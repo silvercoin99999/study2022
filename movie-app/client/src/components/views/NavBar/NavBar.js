2c57ec588c41fbb7759b524c7b25c058345c2970
silvercoin <silvercoin99.999@gmail.com>
2022-10-30 03:18:17 +0900

    movie-app init

---
+++ movie-app/client/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import LeftMenu from "./Sections/LeftMenu";
+import RightMenu from "./Sections/RightMenu";
+import { SwapRightOutlined } from "@ant-design/icons";
+import { Drawer, Button } from "antd";
+import "./Sections/Navbar.css";
+
+function NavBar() {
+  const [open, setOpen] = useState(false);
+
+  const showDrawer = () => {
+    setOpen(true);
+  };
+
+  const onClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <nav
+      className="menu"
+      style={{ position: "fixed", zIndex: 5, width: "100%" }}
+    >
+      <div className="menu__logo">
+        <a href="/">Logo</a>
+      </div>
+      <div className="menu__container">
+        <div className="menu_left">
+          <LeftMenu mode="horizontal" />
+        </div>
+        <div className="menu_rigth">
+          <RightMenu mode="horizontal" />
+        </div>
+        <Button
+          className="menu__mobile-button"
+          type="primary"
+          onClick={showDrawer}
+        >
+          <SwapRightOutlined />
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          className="menu_drawer"
+          closable={false}
+          onClose={onClose}
+          open={open}
+        >
+          <LeftMenu mode="inline" />
+          <RightMenu mode="inline" />
+        </Drawer>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
---
+++ movie-app/server/index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const config = require("./config/key");
+// const { auth } = require("./middleware/auth");
+// const { User } = require("./models/User");
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connect..."))
+  .catch((err) => console.log(err));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/uploads", express.static("uploads"));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`);
+});
